2f10be7e1d2a9235a39f7b90578e575545231c5d
felypen <146992891+felypen@users.noreply.github.com>
2025-08-04 11:42:26 -0300

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+
+const caixaResultado = document.querySelector('.caixa-resltado');
+
+const textoResultado = document.querySelector('.texto-Resultado');
+
+const lista = [];
