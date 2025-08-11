601457a6156d3d1950024c4fc4399fbac20bbf1c
felypen <146992891+felypen@users.noreply.github.com>
2025-08-11 08:14:02 -0300

    .

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
+
 const caixaPrincipal = document.querySelector('.caixa-principal');
 
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
 
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 
-const caixaResultado = document.querySelector('.caixa-resltado');
+const caixaResultado = document.querySelector('.caixa-resultado');
 
 const textoResultado = document.querySelector('.texto-Resultado');
 
 const lista = [];
 
-const perguntas[
-    {
-        enunciado: "Pergunta 1",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativas 2"
-        ]
-    }, // fecha objeto perguntas
-]// fecha lista
- {
-        enunciado: "Pergunta 2",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativas 2"
-        ]
-    }
+const perguntas = [
+{
+enunciado: "Pergunta 1",
+alternativas: ["Alternativa 1","Alternativa 2"]
+},
+{
+enunciado: "Pergunta 2",
+alternativas: ["Alternativa 1","Alternativa 2" ]
+},
+{
+enunciado: "Pergunta 3",
+alternativas: ["Alternativa 1","Alternativa 2" ]
+},
+{
+enunciado: "Pergunta 4",
+alternativas: ["Alternativa 1","Alternativa 2"]
+},
+{
+enunciado: "Pergunta 5",
+alternativas: ["Alternativa 1","Alternativa 2"]
+}
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPerguntas(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+} 
+
+mostraPerguntas
